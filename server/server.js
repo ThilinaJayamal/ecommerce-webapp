9b4b0f5c74466eaeefc5ab9584728cd4d5799240
ThilinaJayamal <thilinajayamal271@gmail.com>
2025-05-23 19:53:42 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.js";
+import userRoute from "./routes/userRoute.js";
+import { authUser } from "./middleware/authUser.js";
+import sellerRoute from "./routes/sellerRoute.js"
+import connectCloudinary from "./config/cloudinary.js";
+import productRoute from "./routes/productRoute.js";
+import cartRoute from "./routes/cartRoute.js";
+import addressRoute from "./routes/addressRoute.js";
+import orderRoute from "./routes/orderRoute.js";
+import { stripeWebhooks } from "./controllers/orderController.js";
+
+const app = express();
+
+const allowedOrigins = ['http://localhost:5173'];
+
+//create DB connection
+await connectDB(process.env.MONGODB_URI);
+await connectCloudinary();
+
+//middleware configaration
+app.use(cors({
+    credentials: true,
+    origin: allowedOrigins
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/user", userRoute);
+app.use("/api/seller", sellerRoute);
+app.use("/api/product", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/address", addressRoute);
+app.use("/api/order", orderRoute);
+
+app.use(authUser)
+
+app.post("/stripe",express.raw({type:"application/json"}),stripeWebhooks);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("server is running on port:", port);
+})
